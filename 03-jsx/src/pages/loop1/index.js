10664dc65413cc93534ee3c215b34878dc241a76
jinju <tlawlswn28@naver.com>
2024-11-19 14:20:21 +0900

    03 JSX | 기본 표현식,  조건문,  반복문 예제 실습

---
+++ 03-jsx/src/pages/loop1/index.js
@@ -0,0 +1,25 @@
+/**
+ * jsx 반복 처리 (1) - 함술르 통한 반복문 활용
+ */
+import React from "react";
+
+const Loop1 = () => {
+  const createListItems = (count) => {
+    let li = [];
+
+    for (let i = 0; i < count; i++) {
+      li.push(<li key={i}>List Item {i}</li>);
+    }
+
+    return li;
+  };
+
+  return (
+    <div>
+      <h1>Loop1</h1>
+      <ul>{createListItems(5)}</ul>
+    </div>
+  );
+};
+
+export default Loop1;
