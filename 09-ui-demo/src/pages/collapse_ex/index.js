824894f4c53c308bce39e1805b1c6f610ba178d3
jinju <tlawlswn28@naver.com>
2024-12-02 11:54:01 +0900

    09 UI Demo | CollapseEx - menuTitle 클릭 시 Content 나오는 간단한 예제
    - toggle을 사용해 active 클래스 적용 여부 변경

--- 09-ui-demo/src/pages/collapse_ex/index.js
+++ 09-ui-demo/src/pages/collapse_ex/index.js
@@ -1,13 +1,78 @@
-import React, { memo } from "react";
+import React, { memo, useCallback } from "react";
 
 import styled from "styled-components";
 
-const CollapseExContainer = styled.div``;
+import { collapseContent } from "../../dataset";
+
+const CollapseExContainer = styled.div`
+  .collapse-title {
+    background-color: #f1f1f1;
+    color: black;
+    cursor: pointer;
+    padding: 22px;
+    box-sizing: border-box;
+    margin: 0;
+    font-size: 16px;
+
+    &:hover {
+      background-color: #ddd;
+    }
+
+    /** 현재 열러 있는 항목을 표시하기 위한 클래스 */
+    &.active {
+      background-color: #ddd;
+    }
+  }
+
+  .collapse-content {
+    background-color: #f1f1f1;
+    font-size: 20px;
+
+    p {
+      padding: 20px 40px;
+      margin: 0;
+      height: auto;
+    }
+
+    height: 0;
+    overflow: hidden;
+    transition: height 0.2s ease-out;
+  }
+`;
 
 const CollapseEx = memo(() => {
+  const onCollapseTitleClick = useCallback((e) => {
+    // 클릭 된 자기 자신
+    const current = e.currentTarget;
+    // 스스로에게 active 클래스에 대한 적용 여부 변경
+    current.classList.toggle("active");
+
+    // 제어할 대상을 탐색
+    const content = current.parentElement.querySelector(".collapse-content");
+
+    // 스스로가 active 클래스가 적용된 상태라면?
+    if (current.classList.contains("active")) {
+      // 높이를 자식 요소의 높이로 변경
+      content.style.height = `${content.scrollHeight}px`;
+    } else {
+      // 높이를 0으로 변경
+      content.style.height = "0";
+    }
+  });
   return (
     <CollapseExContainer>
       <h2>CollapseEx</h2>
+
+      {collapseContent.map(({ title, content }, i) => {
+        return (
+          <div key={i}>
+            <h1 className="collapse-title" onClick={onCollapseTitleClick}>
+              {title}
+            </h1>
+            <h1 className="collapse-content">{content}</h1>
+          </div>
+        );
+      })}
     </CollapseExContainer>
   );
 });
