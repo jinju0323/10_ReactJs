18efa6f2533cfe687cee710d23c6fd1d4c2e5764
jinju <tlawlswn28@naver.com>
2024-12-02 14:35:42 +0900

    10 Simple Ajax | 백엔드 서버에 있는 News Data 불러와서 카드형/리스트형으로 렌더링

--- 10-simple-ajax/src/pages/news/index.js
+++ 10-simple-ajax/src/pages/news/index.js
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import NewsCard from "./NewsCard";
 import NewsList from "./NewsList";
+
 import axios from "axios";
 import axiosHelper from "../../helpers/AxiosHelper";
 
+// 이전 수업에서 구현한 방식
 // import NewsData from "../../data/NewsData";
 
 const News = () => {
+  // 이번 수업에서 구현할 방식
   const [newsData, setNewsData] = useState([]);
+
+  /** 페이지가 처음 열렸을 때 실행할 hook */
+  // hook에 전달되는 콜백함수에 직접적으로 async를 사용할 수 없기 때문에
+  // 별도의 함수를 만들어서 사용한다.
+  useEffect(() => {
+    (async () => {
+      // ajax 결과를 저장할 변수
+      let data = null;
+
+      try {
+        data = await axiosHelper.get("/news");
+      } catch (e) {
+        alert(e.message);
+        return;
+      }
+
+      console.log(data.item);
+
+      // Ajax의 결과에서 화면에 출력할 내용을 상태변수에 적용 --> 화면에 자동 갱신됨
+      setNewsData(data.item);
+    })();
+  }, []);
 
   return (
     <div>
