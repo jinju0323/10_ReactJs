5b65bf4cd4e67ef06ca3f3881733cc33324f4745
jinju <tlawlswn28@naver.com>
2024-12-04 12:44:26 +0900

    11 Simple CRUD | 학과관리 검색기능개선 - useNavigate,  useLocation
    - Ajax의 단점 보완하는 코드 개선
    - QueryString으로 URL 변조되면 컴포넌트 재실행

--- 11-simple-crud/src/pages/department/index.js
+++ 11-simple-crud/src/pages/department/index.js
@@ -2,8 +2,8 @@ import React, { memo, useState, useCallback, useEffect } from "react";
 
 import styled from "styled-components";
 
-// Ajax 처리를 위한 AxiosHelper 참조
 import axiosHelper from "../../helpers/AxiosHelper";
+import utilHelper from "../../helpers/UtilHelper";
 
 import Table from "../../components/Table";
 import Spinner from "../../components/Spinner";
@@ -26,13 +26,25 @@ const DepartmentContainer = styled.div`
 `;
 
 const Department = memo(() => {
+  // 화면에 표시할 상태값(Ajax 연동 결과로 받아올 json) --> 초기값을 빈 배열로 정의
+  // Ajax 처리는 비동기이므로 데이터를 받아오는 처리의 완료 여부와 상관없이 화면 출력이 먼저 수행
+  // 그러므로 Ajax의 결과를 상태값에 저장하여 데이터를 받아온 후 화면이 자동 갱신되도록 처리
   const [department, setDepartment] = useState([]);
 
+  // 현재 Ajax 데이터를 로딩중인지를 의미하는 상태값
   const [loading, setLoading] = useState(false);
+
+  // 페이지 강제 이동을 위한 객체 생성
+  const navigate = useNavigate();
 
   /** 검색 기능 type (1) */
   // 검색어를 저장하기 위한 상태변수
-  const [keyword, setKeyword] = useState("");
+  // const [keyword, setKeyword] = useState("");
+
+  /** 검색 기능 type (2) */
+  // QueryString에 포함된 keyword 값을 취득
+  const { search } = useLocation();
+  const { keyword } = utilHelper.getQuery(search);
 
   /** 페이지가 처음 열렸을 떄 실행할 hook */
   // hook에 전달되는 콜백 함수에 직접적으로 async를 적용할 수 없다.
@@ -51,15 +63,20 @@ const Department = memo(() => {
       }
 
       try {
+        // React Router에 정의되지 않고, public 폴더에도 맵핑되는 경로가 없을 경우
+        // package.json에 설정된 proxy 경로를 기준으로 Ajax 요청을 보낸다.
+        // 즉 --> http://localhost:8080/department
         date = await axiosHelper.get("/department", args);
         console.log(date);
       } catch (e) {
         console.error(e);
         alert(e.message);
       } finally {
+        // Ajax 로딩 종료를 알림
         setLoading(false);
       }
 
+      // Ajax의 요청 결과를 상태값에 저장
       // id 값을 기준으로 정렬
       setDepartment(date.item.sort((a, b) => a.id - b.id));
     })();
@@ -76,7 +93,14 @@ const Department = memo(() => {
     const keyword = form.keyword.value;
     console.log(`검색어 : ${keyword}`);
 
-    setKeyword(keyword);
+    // setKeyword(keyword);
+
+    // 검색어를 QueryString으로 지정하여 페이지를 이동한다.
+    // 실제로는 페이지 이동이 아니라 URL 변조만 이루어 진다.
+    // 하지만 웹 브라우저는 주소가 변경되었으므로 이동으로 인식한다.
+    // --> 페이지 이동으로 인식되면 리액트는 화면상의 모든 컴포넌트를 다시 랜더링한다.
+    // --> 즉, URL이 변경되면 화면에 표시되는 컴포넌트가 처음부터 재실행된다는 의미다.
+    navigate(`/department?keyword=${keyword}`);
   }, []);
 
   return (
