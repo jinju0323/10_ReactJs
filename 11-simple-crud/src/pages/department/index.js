aabbf90a531a9d4c5fdb1433f88244d57331ca5d
jinju <tlawlswn28@naver.com>
2024-12-05 14:21:16 +0900

    11 Simple CRUD | 학과관리 수정버튼 CRUD PUT 구현

--- 11-simple-crud/src/pages/department/index.js
+++ 11-simple-crud/src/pages/department/index.js
@@ -36,6 +36,9 @@ const Department = memo(() => {
   // 현재 Ajax 데이터를 로딩중인지를 의미하는 상태값
   const [loading, setLoading] = useState(false);
 
+  // 수정할 항목에 대한 id값을 저장하기 위한 상태변수
+  const [updateId, setUpdateId] = useState(-1);
+
   // 페이지 강제 이동을 위한 객체 생성
   const navigate = useNavigate();
 
@@ -217,6 +220,77 @@ const Department = memo(() => {
     [department]
   );
 
+  /** 데이터 수정 버튼 click 이벤트 */
+  const onDataEditClick = useCallback((e) => setUpdateId(parseInt(e.currentTarget.dataset.id)), []);
+
+  /** 데이터 수정 submit 이벤트 */
+  const onDataEditSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      // 이벤트가 발생한 <form>요소 취득
+      const current = e.currentTarget;
+
+      // 입력값에 대한 유효성 검사
+      try {
+        regexHelper.value('#edit-form input[name="dname"]', "학과명을 입력하세요.");
+        regexHelper.maxLength('#edit-form input[name="dname"]', 20, "학과명은 20자 이내로 입력하세요.");
+        regexHelper.value('#edit-form input[name="loc"]', "학과위치를 입력하세요.");
+        regexHelper.maxLength('#edit-form input[name="loc"]', 20, "학과위치는 20자 이내로 입력하세요.");
+      } catch (e) {
+        alert(e.message);
+        e.element.focus();
+        return;
+      }
+
+      // form 요소 내의 input 요소들을 name 속성값으로 접근하여 입력값을 얻음
+      const id = parseInt(current.id.value);
+      const dname = current.dname.value;
+      const loc = current.loc.value;
+      console.log(`수정할 데이터 : ${id}, ${dname}, ${loc}`);
+
+      // 백엔드에 데이 터 수정 요청을 보낸다.
+      (async () => {
+        // Ajax 로딩 시작을 알림
+        setLoading(true);
+
+        // 수정 결과에 대한 json을 받을 변수 준비
+        let data = null;
+
+        // 예외처리
+        try {
+          data = await axiosHelper.put(`/department/${id}`, {
+            dname: dname,
+            loc: loc,
+          });
+
+          console.group("데이터 수정 결과");
+          console.log(data);
+          console.groupEnd();
+        } catch (e) {
+          console.error(e);
+          alert(e.message);
+          return;
+        } finally {
+          // Ajax 로딩 종료를 알림
+          setLoading(false);
+        }
+
+        // 현재 출력중인 상태변수 department에 수정된 항목을 교체해야 한다.
+        // useEffect 안에서 상태변수를 사용해야 하므로 종속변수 배열에 department를 추가한다.
+        const editId = department.findIndex((v, i) => v.id === id);
+        console.log(`수정할 데이터의 인덱스 : ${editId}`);
+        const newData = [...department];
+        newData.splice(editId, 1, data.item);
+        console.log(newData);
+        setDepartment(newData);
+
+        // 상태변수를 되돌린다.
+        setUpdateId(-1);
+      })();
+    },
+    [department]
+  );
   return (
     <DepartmentContainer>
       <h2>Department</h2>
@@ -235,38 +309,67 @@ const Department = memo(() => {
         <button type="submit">검색</button>
       </form>
 
-      <Table>
-        <thead>
-          <tr>
-            <th>학과번호</th>
-            <th>학과명</th>
-            <th>학과위치</th>
-            <th>수정</th>
-            <th>삭제</th>
-          </tr>
-        </thead>
-        <tbody>
-          {department.map((v, i) => {
-            return (
-              <tr key={v.id}>
-                <td>{v.id}</td>
-                {keyword ? (
-                  <td dangerouslySetInnerHTML={{ __html: v.dname.replaceAll(keyword, `<mark>${keyword}</mark>`) }}></td>
-                ) : (
-                  <td>{v.dname}</td>
-                )}
-                <td>{v.loc}</td>
-                <td>수정</td>
-                <td>
-                  <button type="button" data-id={v.id} data-dname={v.dname} onClick={onDataDeleteClick}>
-                    삭제
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      {/* 수정처리를 위해서 목록 전체를 form으로 감싼다. */}
+      <form onSubmit={onDataEditSubmit} id="edit-form">
+        <Table>
+          <thead>
+            <tr>
+              <th>학과번호</th>
+              <th>학과명</th>
+              <th>학과위치</th>
+              <th>수정</th>
+              <th>삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            {department.map((v, i) => {
+              if (updateId === v.id) {
+                return (
+                  <tr key={`edit-${v.id}`}>
+                    <td>
+                      {/* `value`속성은 반드시 onChange 이벤트와 함께 사용해야함 */}
+                      {/* onChange 없이 사용할 경우 `defaultValue` 속성을 사용 */}
+                      <input type="hidden" name="id" defaultValue={v.id} />
+                      {v.id}
+                    </td>
+                    <td>
+                      <input type="text" name="dname" defaultValue={v.dname} />
+                    </td>
+                    <td>
+                      <input type="text" name="loc" defaultValue={v.loc} />
+                    </td>
+                    <td colSpan="2">
+                      <button type="submit">수정</button>
+                    </td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr key={v.id}>
+                    <td>{v.id}</td>
+                    {keyword ? (
+                      <td dangerouslySetInnerHTML={{ __html: v.dname.replaceAll(keyword, `<mark>${keyword}</mark>`) }}></td>
+                    ) : (
+                      <td>{v.dname}</td>
+                    )}
+                    <td>{v.loc}</td>
+                    <td>
+                      <button type="button" data-id={v.id} onClick={onDataEditClick}>
+                        수정
+                      </button>
+                    </td>
+                    <td>
+                      <button type="button" data-id={v.id} data-dname={v.dname} onClick={onDataDeleteClick}>
+                        삭제
+                      </button>
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </Table>
+      </form>
     </DepartmentContainer>
   );
 });
