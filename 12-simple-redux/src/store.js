4d9d32dcf3abb7f3007387b0eb9d55eaeee51f5f
jinju <tlawlswn28@naver.com>
2024-12-09 13:02:13 +0900

    12 Simple Redux | 2005~2018년도 교통사고 자료 백엔드 연동해서 테이블 만들기
    - ReduxHelper : Redux의 간소화
    - toLocaleString() : 3자리마다 콤마(,)

--- 12-simple-redux/src/store.js
+++ 12-simple-redux/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import CounterSlice from "./slices/CounterSlice";
 import GradeSlice from "./slices/GradeSlice";
+import TrafficAccSlice from "./slices/TrafficAccSlice";
 
 const store = configureStore({
   // 리듀서 설정 --> Slice 객체들을 나열하여 설정
@@ -10,6 +11,7 @@ const store = configureStore({
   reducer: {
     CounterSlice,
     GradeSlice,
+    TrafficAccSlice,
   },
   // 직렬화 가능한 값을 체크하는 미들웨어를 사용하지 않는다. --> for Ajax
   middleware: (getDefaultMiddleware) =>
